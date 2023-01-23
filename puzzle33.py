c45263c62da0696263f7b2339ab26d517da5c975
sophia-faye <jjohnbonjuvi@gmail.com>
2023-01-23 08:17:47 -0800

    putting infinite condition

--- puzzle33.py
+++ puzzle33.py
@@ -3,9 +3,9 @@ def bubble_sort(var):
     for border in range (len(var) -1, 0, -1):
         for i in range(border):
             if var[i] > var[i+1]:
-                pass
+                while True:
+                    print(var[i])
     return var
 
 listfor_sorting = [10, 3, 4, 6, 11, 22, 34]
 
-    
